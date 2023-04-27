3b3e77d34922ac66df743fb42442ccee7516e3d5
Mihizz <86364159+Mihizz@users.noreply.github.com>
2023-04-28 00:13:26 +0200

    Konv Valute

---
+++ KonvertovanjeValuta/src/konvertorvaluta/KonvertorValuta.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konvertorvaluta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import konvertorvaluta.Valuta;
+
+/**
+ *
+ * @author Nemanja
+ */
+public class KonvertorValuta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Valuta> valute = new ArrayList<>();
+        valute.add(new Valuta("Euro", "EUR", 117.231));
+        valute.add(new Valuta("Americki dolar", "USD", 105.771));
+        valute.add(new Valuta("Svajcarski franak", "CHF", 118.672));
+        valute.add(new Valuta("Britanska funta", "GBP", 131.917));
+        valute.add(new Valuta("BIH konvertabilna marka", "BAM", 59.830));
+
+        while (true) {
+            System.out.println("--- Dobrosli u konvertor valuta --- \nUnesite cifru ispred opcije za start");
+            System.out.println("1 - Konvertuj iz dinara u devize");
+            System.out.println("2 - Konvertuj iz deviza u dinare");
+            System.out.println("3 - Izadji iz programa");
+            System.out.print("Zelim opciju: ");
+            int opcija = sc.nextInt();
+
+            switch (opcija) {
+                case (1):
+                    System.out.println("-------------------");
+                    System.out.print("Unesite iznos dinara: ");
+                    double dinar = sc.nextDouble();
+
+                    System.out.println("-------------------");
+                    System.out.println("Unesite jednu od oznaka deviza sa spiska:");
+
+                    for (int i = 0; i < valute.size(); i++) {
+                        System.out.println((i + 1) + ". " + valute.get(i).toString());
+                    }
+
+                    System.out.print("Oznaka: ");
+                    String oznaka = sc.next();
+
+                    if (Valuta.proveriOznaku(oznaka, valute)) {
+                        Valuta val = null;
+                        for (int i = 0; i < valute.size(); i++) {
+                            if (valute.get(i).getOznaka().equalsIgnoreCase(oznaka)) {
+                                val = valute.get(i);
+                            }
+                        }
+
+                        System.out.println("-------------------");
+                        System.out.println("Vas iznos je: " + Valuta.konvertujUDevizu(dinar, val) + " " + val.getOznaka());
+                        System.out.println("-------RESET-------");
+                    } else {
+                        System.out.println("-------------------");
+                        System.out.println("Niste uneli vazecu valutu!!!");
+                        System.out.println("-------RESET-------");
+                    }
+                    break;
+                case (2):
+                    System.out.println("-------------------");
+                    System.out.println("Unesite jednu od oznaka deviza sa spiska:");
+
+                    for (int i = 0; i < valute.size(); i++) {
+                        System.out.println((i + 1) + ". " + valute.get(i).toString());
+                    }
+
+                    System.out.print("Oznaka: ");
+                    String oznaka1 = sc.next();
+
+                    if (Valuta.proveriOznaku(oznaka1, valute)) {
+
+                        System.out.println("-------------------");
+                        System.out.print("Unesite iznos deviza: ");
+                        double dinar1 = sc.nextDouble();
+
+                        Valuta val = null;
+                        for (int i = 0; i < valute.size(); i++) {
+                            if (valute.get(i).getOznaka().equalsIgnoreCase(oznaka1)) {
+                                val = valute.get(i);
+                            }
+                        }
+
+                        System.out.println("-------------------");
+                        System.out.println("Vas iznos je: " + Valuta.konvertujUDinare(dinar1, val) + " DIN");
+                        System.out.println("-------RESET-------");
+                    } else {
+                        System.out.println("-------------------");
+                        System.out.println("Niste uneli vazecu valutu!!!");
+                        System.out.println("-------RESET-------");
+                    }
+                    break;
+                case (3):
+                    break;
+                default:
+                    System.out.println("-------------------");
+                    System.out.println("Pogresan unos!!!");
+                    System.out.println("-------RESET-------");
+            }
+            if (opcija == 3) {
+                break;
+            }
+
+        }
+
+    }
+
+}
---
+++ KonvertovanjeValuta/src/konvertorvaluta/Valuta.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konvertorvaluta;
+
+import java.util.List;
+
+/**
+ *
+ * @author Nemanja
+ */
+public class Valuta {
+    private String naziv;
+    private String oznaka;
+    private double kurs;
+
+    public Valuta(String naziv, String oznaka, double kurs) {
+        this.naziv = naziv;
+        this.oznaka = oznaka;
+        this.kurs = kurs;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+
+    public double getKurs() {
+        return kurs;
+    }
+
+    public void setKurs(double kurs) {
+        this.kurs = kurs;
+    }
+
+    @Override
+    public String toString() {
+        return oznaka + " - " + naziv;
+    }
+    
+    public static boolean proveriOznaku(String oznaka,List<Valuta> valute){
+        for (int i = 0; i < valute.size(); i++) {
+            if(valute.get(i).getOznaka().equalsIgnoreCase(oznaka)){
+                return true;
+            }
+        }
+        return false;
+    }
+       
+    public static double konvertujUDevizu(double dinar, Valuta val){
+        double deviza = dinar / val.kurs;
+        return(zakruziNa1Decimalu(deviza));
+    }
+    
+    public static double konvertujUDinare(double deviza, Valuta val){
+        double dinar = deviza * val.kurs;
+        return(zakruziNa1Decimalu(dinar));
+    }
+    
+    public static double zakruziNa1Decimalu(double iznos)
+    {
+        return Math.round(iznos * 10.0) / 10.0;
+    }    
+    
+    
+}
